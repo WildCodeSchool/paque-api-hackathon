5b04ffd37dc38ac096e307304c90b3fb6f4aba24
mdhn <maxduchan@gmail.com>
2019-03-27 16:43:34 +0100

    remove, update, delete

--- api/character/character.service.js
+++ api/character/character.service.js
@@ -4,20 +4,35 @@ function save(fields) {
     return characters().insert(fields);
 }
 
+function update(id, fields) {
+    return characters().update(fields);
+}
+
 function get(id) {
-    return characters().get(id);
+    return characters().findOne({'id.$oid': id});
+}
+
+function remove(id) {
+    return characters().findAndRemove({'id.$oid': id})
 }
 
 function random() {
     const size = characters().count();
+    const skip = randomIndex(1, size);
 
-    const index = Math.floor(Math.random() * size);
+    const character = characters().chaine().findOne().offset(skip);
 
-    return characters().get(index);
+    return character;
 }
 
 function getAll() {
     return characters().find({});
 }
 
-module.exports = { save, get, getAll, random}
+function randomIndex(min, max) {
+    const value = Math.random() * (+max - +min) + +min;
+
+    return value
+}
+
+module.exports = { save, get, getAll, update, remove, random}
--- api/character/index.js
+++ api/character/index.js
@@ -7,15 +7,6 @@ router.get('', function(req, res) {
     res.status(200).json(characters);
 });
 
-router.get('/:id', function(req, res) {
-    const character = service.get(req.params.id);
-    if (character) {
-        res.status(200).json(character);
-    } else {
-        res.status(404).end();
-    }
-});
-
 router.get('/random', function(req, res) {
     const characters = service.random();
     if (characters) {
@@ -25,9 +16,28 @@ router.get('/random', function(req, res) {
     }
 });
 
+router.get('/:id', function(req, res) {
+    const character = service.get(req.params.id);
+    if (character) {
+        res.status(200).json(character);
+    } else {
+        res.status(404).end();
+    }
+});
+
 router.post('', function(req, res) {
     const character = service.save(req.body);
     res.status(201).json(character);
+});
+
+router.put('/:id', function(req, res) {
+    const character = service.update(req.params.id, req.body);
+    res.status(200).json(character);
+});
+
+router.delete('/:id', function(req, res) {
+    const character = service.remove(req.params.id);
+    res.status(204).json(character);
 });
 
 module.exports = router;
