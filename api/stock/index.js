0a94e24f3b7c19fc97d77a6b196876131f6e86d9
mdhn <maxduchan@gmail.com>
2019-04-03 20:56:52 +0200

    static stock service for php, java...

--- api/stock/index.js
+++ api/stock/index.js
@@ -2,7 +2,10 @@ const {
     Router
 } = require('express');
 const Stock = require('./stock.service')
-const { EMIT_INTERVAL } = require('../../configuration/environment')
+const StaticStock = require('./static-stock.service')
+const {
+    EMIT_INTERVAL
+} = require('../../configuration/environment')
 
 const router = new Router();
 require('express-ws')(router);
@@ -15,6 +18,25 @@ chocolateStock.startAutoIncrement(EMIT_INTERVAL);
 eggStock.startAutoIncrement(EMIT_INTERVAL);
 milkStock.startAutoIncrement(EMIT_INTERVAL);
 
+
+router.get('/chocolates', extractParams, (req, res) => {
+    const staticStock = new StaticStock(15.86, 125, 0.5, 500, 'g', new Date(req.min));
+    const data = staticStock.startAutoIncrement(EMIT_INTERVAL);
+    res.status(200).json(data)
+});
+
+router.get('/eggs', extractParams, (req, res) => {
+    const staticStock = new StaticStock(4.5, 55, 0.5, 1000, 'g', new Date(req.min));
+    const data = staticStock.startAutoIncrement(EMIT_INTERVAL);
+    res.status(200).json(data)
+});
+
+router.get('/milks', extractParams, (req, res) => {
+    const staticStock = new StaticStock(25, 1000, 1, 1000, 'l', new Date(req.min));
+    const data = staticStock.startAutoIncrement(EMIT_INTERVAL);
+    res.status(200).json(data)
+});
+
 router.ws('/chocolates', (ws, req) => {
     chocolateStock.addClient(ws);
 });
@@ -26,5 +48,25 @@ router.ws('/eggs', (ws, req) => {
 router.ws('/milks', (ws, req) => {
     milkStock.addClient(ws)
 });
+
+function extractParams(req, res, next) {
+    try {
+        req.min = new Date(req.query.min);
+    } catch (err) {
+        res.status(400).json({
+            message: 'Min date must me a valid timestamp'
+        })
+        return;
+    }
+    try {
+        req.interval = req.query.interval ? req.query.interval : EMIT_INTERVAL;
+    } catch (err) {
+        res.status(400).json({
+            message: 'Interval must me a valid number'
+        })
+        return;
+    }
+    next()
+}
 
 module.exports = router;
---
+++ api/stock/static-stock.service.js
@@ -0,0 +1,25 @@
+const Stock = require('./stock.service');
+
+class StaticStock extends Stock {
+    constructor(priceMin, priceMax, step, amount, unit, date) {
+        super(priceMin, priceMax, step, amount, unit);
+        this.date = date;
+    }
+
+    startAutoIncrement(interval) {
+        const today = new Date().getTime();
+        const data = [];
+        while(this.date.getTime() <= today && data.length < 50000) {
+            const increment = this.increment();
+            data.push(increment);
+            this.date.setTime(this.date.getTime() + interval)
+        }
+        return data;
+    }
+
+    getDate() {
+        return new Date(this.date.getTime()).toISOString();
+    }
+}
+
+module.exports = StaticStock;
