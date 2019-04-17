a65f4758ddc71a30d2cfa433e2158be8917d1fa5
mdhn <maxduchan@gmail.com>
2019-04-17 12:42:42 +0200

    increase default interval

--- configuration/environment.js
+++ configuration/environment.js
@@ -1,5 +1,5 @@
 const NODE_PORT = process.env.NODE_PORT ? process.env.NODE_PORT : '3000';
-const EMIT_INTERVAL = process.env.EMIT_INTERVAL ? process.env.EMIT_INTERVAL : '500';
+const EMIT_INTERVAL = process.env.EMIT_INTERVAL ? process.env.EMIT_INTERVAL : '1000';
 
 module.exports.NODE_PORT = NODE_PORT;
 module.exports.EMIT_INTERVAL = EMIT_INTERVAL;
