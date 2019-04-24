6ebf4cb79ec133268621c008c37271dcbfb5fce2
mdhn <maxduchan@gmail.com>
2019-04-24 14:19:38 +0200

    enable https

--- configuration/environment.js
+++ configuration/environment.js
@@ -1,5 +1,7 @@
 const NODE_PORT = process.env.NODE_PORT ? process.env.NODE_PORT : '3000';
 const EMIT_INTERVAL = process.env.EMIT_INTERVAL ? process.env.EMIT_INTERVAL : '1000';
+const ENABLE_HTTPS = process.env.ENABLE_HTTPS ? process.env.ENABLE_HTTPS : true;
 
 module.exports.NODE_PORT = NODE_PORT;
 module.exports.EMIT_INTERVAL = EMIT_INTERVAL;
+module.exports.ENABLE_HTTPS = ENABLE_HTTPS;
--- server.js
+++ server.js
@@ -8,13 +8,15 @@ const {
 const {
     databaseInitialize
 } = require('./configuration/connection');
+const { ENABLE_HTTPS } = require("./configuration/environment")
 const cors = require('cors');
 const swaggerUi = require('swagger-ui-express');
 const expressWs = require('express-ws')(app);
 const swaggerDocument = require('./swagger.json');
 const path = require('path');
-var https = require('https');
-var http = require('http');
+const fs = require('fs');
+const https = require('https');
+const http = require('http');
 
 app.use(cors())
 app.use(bodyParser.json());
@@ -26,7 +28,7 @@ app.use('/api/eggs', require('./api/egg'));
 app.use('/api/characters', require('./api/character'));
 app.use('/api/stocks', require('./api/stock'));
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));    
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.get('/api/db', (req, res) => {
     var file = path.join(__dirname, '   db.json');
@@ -35,25 +37,38 @@ app.get('/api/db', (req, res) => {
 
 app.use('/.well-known', express.static('.well-known', { dotfiles: 'allow' }));
 
-app.use('/api', (req, res) => {
+/* app.use('/api', (req, res) => {
     res.redirect('/api-docs');
 });
 
 app.use('/', (req, res) => {
     res.redirect('/api-docs');
-});
+}); */
 
 async function start() {
     try {
         databaseInitialize();
         // Start the server
-        http.createServer(app).listen(process.env.PORT || NODE_PORT,() => {
+        http.createServer(app).listen(process.env.PORT || NODE_PORT, () => {
             log.debug(`Your server is listening on port ${process.env.PORT || NODE_PORT}`);
         });
 
-        https.createServer(app).listen(443, () => {
-            log.debug(`Your server is listening on port 443`);
-        });
+        if (ENABLE_HTTPS === true) {
+            const privateKey = fs.readFileSync('/etc/letsencrypt/live/easteregg.wildcodeschool.fr/privkey.pem', 'utf8');
+            const certificate = fs.readFileSync('/etc/letsencrypt/live/easteregg.wildcodeschool.fr/cert.pem', 'utf8');
+            const ca = fs.readFileSync('/etc/letsencrypt/live/easteregg.wildcodeschool.fr/chain.pem', 'utf8');
+
+            const credentials = {
+                key: privateKey,
+                cert: certificate,
+                ca: ca
+            };
+
+            https.createServer(credentials, app).listen(443, () => {
+                log.debug(`Your server is listening on port 443`);
+            });
+        }
+
     } catch (err) {
         log.error('while starting server', err);
     }
